68a84cfb8a5ca9a29cffa992e1a36024f107d431
avishay <avishay@ubuntuTF.localdomain>
2024-11-03 00:14:11 +0200

    initial commit

--- testing/app_variables.tf
+++
@@ -1,5 +0,0 @@
-variable "app_container_name" {}
-variable "app_image" {}
-variable "app_int_port" {}
-variable "app_ext_port" {}
-variable "app_image_name" {}
--- testing/nginx_variables.tf
+++
@@ -1,7 +0,0 @@
-variable "nginx_container_name" {}
-variable "nginx_image" {}
-variable "nginx_int_port" {}
-variable "nginx_ext_port" {}
-variable "nginx_image_name" {}
-
-
